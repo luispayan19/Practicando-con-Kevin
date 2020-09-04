f17fda0f16c1514d7c9453f94f9fce634d7e44d9
luispayan19 <69813586+luispayan19@users.noreply.github.com>
2020-09-03 20:49:12 -0400

    Built class and constructor

---
+++ Student.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Student{
+    private:
+    double Student1;
+    double student2;
+    double student3;
+    double student4;
+    double student5;
+    public:
+    Student(double aStudent1, double astudent2, double astudent3, double astudent4, double astudent5){
+        this-> Student1= aStudent1;
+        this->student2= astudent2;
+        this->student3= astudent3;
+        this->student4= astudent4;
+        this->student5= astudent5;
+    }
+};
